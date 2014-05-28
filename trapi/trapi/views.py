f822187d737fbb92598f7af4b03ba1dcbc2f6c49
Lukasz Boldys <mail@utek.pl>
2014-05-28 14:59:45 +0200

    comment

--- trapi/trapi/views.py
+++ trapi/trapi/views.py
@@ -4,6 +4,8 @@ from .models import TrainsCollection
 
 @view_config(context=TrainsCollection, renderer='jsonp')
 def pkp(context, request):
+    # Yup. I know that this here is kinda bad.
+    # Need to fix it ASAP :)
     headers = ("Access-Control-Allow-Origin", request.headers.get("Origin", ""))
     request.response.headerlist.append(headers)
     return context.geodict
