fc324a8de2fbcc76fb61f1abb4dcb1ec3e1a23d4
Łukasz Bołdys <mail@utek.pl>
2014-07-24 09:13:46 +0200

    fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var spawn = require("gulp-spawn");
 var watch = require("gulp-watch");
 var livereload = require("gulp-livereload");
 var plumber = require("gulp-plumber");
+var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');
 
 var EXPRESS_PORT = 4000;
 var EXPRESS_ROOT = __dirname + "/build";
@@ -19,31 +21,43 @@ function startExpress() {
   app.listen(4000);
 }
 
-gulp.task("csslint", function () {
+gulp.task("csslint", function() {
   return gulp.src("pages/*/css/*.css")
     .pipe(csslint())
     .pipe(csslint.reporter());
 });
 
-gulp.task("jslint", function () {
+gulp.task("jslint", function() {
   return gulp.src("pages/*/js/*.js")
     .pipe(jslint())
     .pipe(jslint.reporter("default"));
 });
 
-gulp.task("serve", function () {
+gulp.task("serve", function() {
   startExpress();
   gulp.src(['pages/*/index.html', 'pages/*/js/*.js', 'pages/*/css/*.css'])
-    .pipe(watch(function (files) {
+    .pipe(watch(function(files) {
       return files.pipe(livereload())
     }))
 });
 
-gulp.task("update", function () {
-  gulp.src('../utk-ol3js-controls/build/js/*.js')
-    .pipe(gulp.dest('map/js'));
-  gulp.src('../utk-ol3js-controls/build/css/*.css')
-    .pipe(gulp.dest('map/css'));
+gulp.task("update", function() {
+  gulp.src('libs/utk-ol3js-controls/build/js/*.js')
+    .pipe(gulp.dest('pages/trains/js'));
+  gulp.src('libs/utk-ol3js-controls/build/css/*.css')
+    .pipe(gulp.dest('pages/trains/css'));
 });
 
-gulp.task('default', ['serve'])
+gulp.task('compress-libs', function() {
+  gulp.src('libs/utk-ol3js-controls/src/css/*.css')
+    .pipe(minifyCSS({
+      keepBreaks: true
+    }))
+    .pipe(gulp.dest('libs/utk-ol3js-controls/build/css'));
+  gulp.src('libs/utk-ol3js-controls/src/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('libs/utk-ol3js-controls/build/js'));
+});
+
+gulp.task('default', ['serve']);
+gulp.task('libs', ["compress-libs", "update"]);
